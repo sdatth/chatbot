89cba12d46c4186783ad854668f99ffc796d173a
dattha <dattha@tutamail.com>
2023-11-29 21:19:27 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  organization: "org-WQJJQeoCmA1ALfM6hqTlZ4Is",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a aiGPT. You can help with graphic design tasks",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
